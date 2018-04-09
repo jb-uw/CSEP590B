c54e96cf6fb362f5e8a9072cb5e52644cdb8a7a8
Joyce Brombaugh <27322622+jb-uw@users.noreply.github.com>
2018-04-08 23:21:50 -0700

    Update find_ball.py

--- lab4/find_ball.py
+++ lab4/find_ball.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+# Joyce Brombaugh
+# Lab 4
+
 import cv2
 import sys
 import copy
@@ -21,13 +24,28 @@ def find_ball(opencv_image, debug=False):
 				debugging information is displayed.
 		
 		Returns [x, y, radius] of the ball, and [0,0,0] or None if no ball is found.
-	"""
-
-	ball = None
-	
+	"""	
 	## TODO: INSERT YOUR SOLUTION HERE
+	circles = None
+	# Blur image first
+	opencv_image = cv2.medianBlur(opencv_image,5)
+	if debug:
+		# display the canny image for debugging
+		edges = cv2.Canny(opencv_image,50,100)
+		pil_image = Image.fromarray(edges)
+		pil_image.show()
+	# Apply HughCircles	
+	circles=cv2.HoughCircles(opencv_image,cv2.HOUGH_GRADIENT,2,100,param1=250,param2=60)
 	
-	return ball
+	if circles is not None:
+		if debug:
+			print(str(len(circles[0])) + " circle(s) found")
+			display_circles(opencv_image, circles[0], circles[0][0])
+		return circles[0][0]
+	else:
+		if debug:
+			print("No circle found")
+		return None
 
 
 def display_circles(opencv_image, circles, best=None):
@@ -54,9 +72,9 @@ def display_circles(opencv_image, circles, best=None):
 		cv2.circle(circle_image,(c[0],c[1]),2,(0,255,255),3) 
 		# write coords
 		cv2.putText(circle_image,str(c),(c[0],c[1]),cv2.FONT_HERSHEY_SIMPLEX,
-					.5,(255,255,255),2,cv2.LINE_AA)            
+					.5,(255,255,255),2,cv2.LINE_AA)				
 	
-	#highlight the best circle in a different color
+	# highlight the best circle in a different color
 	if best is not None:
 		# draw the outer circle
 		cv2.circle(circle_image,(best[0],best[1]),best[2],(0,0,255),2)
@@ -66,10 +84,28 @@ def display_circles(opencv_image, circles, best=None):
 		cv2.putText(circle_image,str(best),(best[0],best[1]),cv2.FONT_HERSHEY_SIMPLEX,
 					.5,(255,255,255),2,cv2.LINE_AA)            
 		
-	
-	#display the image
+	# display the image
 	pil_image = Image.fromarray(circle_image)
-	pil_image.show()    
+	pil_image.show()  
 	  
 if __name__ == "__main__":
-	pass
+
+	filenames = ("test01.bmp", "test03.bmp", "test05.bmp", "test07.bmp", "test09.bmp",
+				 "test11.bmp", "test13.bmp", "test15.bmp", "test17.bmp", "test19.bmp",
+				 "test21.bmp", "test23.bmp", "test25.bmp", "test27.bmp", "test29.bmp",
+				 "test31.bmp", "test33.bmp", "test35.bmp", "test37.bmp", "test39.bmp",
+				 "test41.bmp", "test43.bmp", "test45.bmp", "test47.bmp", "test49.bmp",
+				 "test51.bmp", "test53.bmp", "test55.bmp", "test57.bmp", "test59.bmp",
+				 "test61.bmp", "test63.bmp", "test65.bmp", "test67.bmp", "test69.bmp",
+				 "test71.bmp", "test73.bmp", "test75.bmp", "test77.bmp", "test79.bmp",
+				 "test81.bmp", "test83.bmp", "test85.bmp", "test87.bmp", "test89.bmp",
+				 "test91.bmp", "test93.bmp", "test95.bmp", "test97.bmp", "test99.bmp")
+	for file in filenames:
+		print(str(file) + ":")
+		opencv_image = cv2.imread("./imgs/" + file, cv2.COLOR_GRAY2RGB)
+
+		if opencv_image is not None:
+			ball = find_ball(opencv_image, True)
+			print(str(ball))
+		
+		input("Press Enter for next image. Ctrl-C to quit. ")
